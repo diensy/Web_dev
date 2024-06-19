89c2a573f6b76e2ddf3e7624ed45247608319d77
Dinesh <dineshsahoo702@gmail.com>
2024-06-20 00:24:52 +0530

    condition

---
+++ Javascript/condintion/script.js
@@ -0,0 +1,36 @@
+console.log("if else condintion :");
+
+let age = 45;
+
+if (age >= 18) {
+  console.log("You can drive");
+} else {
+  console.log("You can not drive");
+}
+
+console.log("Operators");
+
+let a = 5;
+let b = 6;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b); //power 5 power 6
+console.log(a % b);
+// assignment operator
+let x = 5;
+let y = 5;
+
+if (x == y) {
+  console.log(x * y);
+} else {
+  console.log(x + y);
+}
+
+console.log("terinary operator");
+
+let p = 45;
+let q = 65;
+
+p > q ? console.log("P is max" + p) : console.log("q is max " + q);
