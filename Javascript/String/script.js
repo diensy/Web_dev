a112171ecfc506ac2e05b6161bbfdfb51d265bc6
Dinesh <dineshsahoo702@gmail.com>
2024-06-22 19:35:37 +0530

    String

---
+++ Javascript/String/script.js
@@ -0,0 +1,19 @@
+let a = "Dinesh";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// console.log(a[6]);
+console.log(a.length);
+
+let name = "Rinku";
+let friend = "Satya";
+
+console.log("His name is " + name + " and his friend is " + friend);
+console.log(`His name is ${name} and his friend is ${friend}`);
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
