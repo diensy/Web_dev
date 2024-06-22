04c406d338e51ac856b01a4795d8fb37fcf59d6e
Dinesh <dineshsahoo702@gmail.com>
2024-06-22 20:28:36 +0530

    DOM-2

---
+++ Javascript/DOM/DOM-2/script.js
@@ -0,0 +1,6 @@
+document.body.firstElementChild.firstElementChild.style.backgroundColor = "red";
+document.body.firstElementChild.lastElementChild.style.backgroundColor =
+  "#4848ff";
+document.body.firstElementChild.children[1].style.backgroundColor = "green";
+document.body.firstElementChild.children[2].style.backgroundColor = "yellow";
+document.body.firstElementChild.children[3].style.backgroundColor = "violet";
