fee5b08ebfabafe4ae69c0d27e3d65bf11fe38a0
Dinesh Kumar Sahoo <dineshsahoo702@gmail.com>
2024-08-22 18:40:29 +0530

    Intro

---
+++ React/first/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [value, setValue] = useState(0);
+  return (
+    <div className="App">
+      <div className="value">{value}</div>
+      <button
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
+}
+
+export default App;
